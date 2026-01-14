3375f5e32eb3bdad9c9f65dca3b2972343d9c4c1
kantaphong <solnutkantaphongkaeoket@gmail.com>
2026-01-14 15:34:59 +0700

    แก้อะไรโน๊ตไว้เฉยๆ

--- database/mysql.go
+++ database/mysql.go
@@ -20,12 +20,12 @@ var dbCals *gorm.DB
 func Connect() {
 	var err error
 	db, err = gorm.Open(mysql.New(mysql.Config{
-		DSN:                       "root:@tcp(127.0.0.1:3306)/userproject?charset=utf8&parseTime=True&loc=Local", // data source name
-		DefaultStringSize:         256,                                                                           // default size for string fields
-		DisableDatetimePrecision:  true,                                                                          // disable datetime precision, which not supported before MySQL 5.6
-		DontSupportRenameIndex:    true,                                                                          // drop & create when rename index, rename index not supported before MySQL 5.7, MariaDB
-		DontSupportRenameColumn:   true,                                                                          // `change` when rename column, rename column not supported before MySQL 8, MariaDB
-		SkipInitializeWithVersion: false,                                                                         // auto configure based on currently MySQL version
+		DSN:                       "surindev_healthy:@admin12345@tcp(127.0.0.1:3306)/surindev_healthy?charset=utf8&parseTime=True&loc=Local", // data source name
+		DefaultStringSize:         256,                                                                                                       // default size for string fields
+		DisableDatetimePrecision:  true,                                                                                                      // disable datetime precision, which not supported before MySQL 5.6
+		DontSupportRenameIndex:    true,                                                                                                      // drop & create when rename index, rename index not supported before MySQL 5.7, MariaDB
+		DontSupportRenameColumn:   true,                                                                                                      // `change` when rename column, rename column not supported before MySQL 8, MariaDB
+		SkipInitializeWithVersion: false,                                                                                                     // auto configure based on currently MySQL version
 	}), &gorm.Config{})
 	if err != nil {
 		fmt.Println("DB Connect Error:", err)
